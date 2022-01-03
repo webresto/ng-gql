1e984aa1a371da0dd422baa166d5c4fe886b114b
Axrl <axrl@mail.ru>
2022-01-03 18:50:55 +0300

    add reexport indexes

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,13 +1,8 @@
 /*
  * Public API Surface of ng-gql
  */
-
-export * from './lib/cart-dish/cart-dish';
-export * from './lib/cart/cart';
-export * from './lib/cart/order';
-export * from './lib/cart/check-phone-response';
-export * from './lib/cart/check-response';
-export * from './lib/cart/phone';
+export * from './lib/cart';
+export * from './lib/cart-dish/cart-dish';;
 export * from './lib/dish/dish';
 export * from './lib/group/group';
 export * from './lib/modifier/modifier';
@@ -16,16 +11,8 @@ export * from './lib/payment-method/payment-method';
 
 export * from './lib/ng-gql.service';
 export * from './lib/ng-gql.module';
-export * from './lib/services/ng-cart.service';
-export * from './lib/services/eventer.service';
+export * from './lib/services';
 export * from './lib/event-message/event-message';
 
 
-export * from './lib/directives/add-dish-to-cart.directive';
-export * from './lib/directives/amount-cart.directive';
-export * from './lib/directives/delete-from-cart.directive';
-export * from './lib/directives/order-cart-user.directive';
-export * from './lib/directives/set-amount.directive';
-export * from './lib/directives/dish-calc.directive';
-export * from "./lib/directives/checkout.directive";
-export * from './lib/directives/set-dish-comment.directive';
+export * from './lib/directives';
