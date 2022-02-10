cabd0447cea449e8e66620f82e5be823f8dd20b3
Axrl <axrl@mail.ru>
2022-02-10 11:53:38 +0300

    fix null type upd deps

--- projects/webresto/ng-gql/src/lib/directives/add-dish-to-order.directive.ts
+++ projects/webresto/ng-gql/src/lib/directives/add-dish-to-order.directive.ts
@@ -2,21 +2,17 @@ import { Directive, HostListener, Input, Output, EventEmitter } from '@angular/c
 import type { AddToOrderInput, Order, Dish, OrderModifier } from '../models';
 import { NgOrderService } from '../services/ng-order.service';
 
-@Directive({
+@Directive( {
   selector: '[addToOrder]'
-})
+} )
 export class AddDishToOrderDirective {
 
-  order: Order | undefined;
+  order: Order | null | undefined;
   modifiers: OrderModifier[] = [];
 
-  constructor(private orderService: NgOrderService) {
-    this.orderService
-      .userOrder$()
-      .subscribe(res => this.order = res);
-    this.orderService
-      .getModifiers()
-      .subscribe(res => this.modifiers = res);
+  constructor ( private orderService: NgOrderService ) {
+    this.orderService.userOrder$().subscribe( res => this.order = res );
+    this.orderService.getModifiers().subscribe( res => this.modifiers = res );
   }
 
   @Input() dish: Dish | undefined;
@@ -28,12 +24,12 @@ export class AddDishToOrderDirective {
   @Output() success = new EventEmitter<boolean>();
   @Output() error = new EventEmitter<any>();
 
-  @HostListener('click')
-  onClick() {
-    this.addDishToOrder(this.dish!.id, this.amountDish)
+  @HostListener( 'click' )
+  onClick () {
+    this.addDishToOrder( this.dish!.id, this.amountDish );
   }
 
-  private addDishToOrder(dishID: string, amount: number) {
+  private addDishToOrder ( dishID: string, amount: number ) {
 
     let data: AddToOrderInput = {
       "dishId": dishID,
@@ -45,15 +41,15 @@ export class AddDishToOrderDirective {
       "orderDishId": this.order?.id
     };
 
-    this.loading.emit(true);
+    this.loading.emit( true );
 
     this.orderService
-      .addDishToOrder$(data)
+      .addDishToOrder$( data )
       .subscribe(
-        _ => this.success.emit(true),
-        e => this.error.emit(e),
+        _ => this.success.emit( true ),
+        e => this.error.emit( e ),
         () => {
-          this.loading.emit(false)
+          this.loading.emit( false );
         }
       );
   }
